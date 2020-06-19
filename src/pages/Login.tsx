bc967c8e5f25f0dc247452a706cce3b85e5eac83
Shakil Ahmed <shakil147258@gmail.com>
2020-06-19 14:34:35 +0600

    feat: add login component

---
+++ src/pages/Login.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import logo from "../logo.svg";
+import "./styles/index.css";
+export const Login = () => {
+  return (
+    <>
+      <Container>
+        <Row className="justify-content-md-center mt-4">
+          <Col md="4">
+            <Card style={{ width: "auto" }}>
+              <Card.Img variant="top" src={logo} className="loginPageLogo" />
+              <Card.Body>
+                <Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
