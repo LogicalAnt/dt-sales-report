dbca7056894060361e01790a3a68606f0cbd82a8
Shakil Ahmed <shakil147258@gmail.com>
2020-06-21 13:01:22 +0600

    feat: add barchart component

--- src/components/Charts/BarChart.tsx
+++ src/components/Charts/BarChart.tsx
@@ -1,5 +1,53 @@
 import React from "react";
+import { Bar } from "react-chartjs-2";
+//import { apidata } from "./data";
 
-export const BarChart = () => {
-  return <div>bar chart</div>;
+//const prods = new Set();
+
+// apidata.map((item) => {
+//   return prods.add(item.product);
+// });
+//["MotoTracker", "CarTracker", "AssetLock", "EagleCam"]
+
+// const data = {
+//   labels: ["Jan", "Feb", "Mar", "Apr"],
+//   datasets: [
+//     {
+//       label: "MotoTracker",
+//       backgroundColor: "#28a745",
+//       data: [15, 20, 30, 40],
+//     },
+//     {
+//       label: "CarTracker",
+//       backgroundColor: "#000",
+//       data: [20, 40, 15, 45],
+//     },
+//     {
+//       label: "AssetLock",
+//       backgroundColor: "#f63a3a",
+//       data: [25, 14, 25, 30],
+//     },
+//     {
+//       label: "EagleCam",
+//       backgroundColor: "#a63a3a",
+//       data: [17, 12, 47, 15],
+//     },
+//   ],
+// };
+interface BarDataType {
+  labels: Array<any>;
+  datasets: Array<object>;
+}
+
+export const BarChart = ({ labels, datasets }: BarDataType) => {
+  return (
+    <Bar
+      data={{ labels, datasets }}
+      width={250}
+      height={250}
+      options={{
+        maintainAspectRatio: false,
+      }}
+    />
+  );
 };
