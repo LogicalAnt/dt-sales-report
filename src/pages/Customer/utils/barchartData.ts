f1fd829fb42da630a363138f0cb28d7ac9a692ed
Shakil Ahmed <shakil147258@gmail.com>
2020-06-21 13:02:51 +0600

    feat: add barchart data manipulation function

---
+++ src/pages/Customer/utils/barchartData.ts
@@ -0,0 +1,80 @@
+import { apidata } from "../../../components/Charts/data";
+interface SaleProps {
+  date: string;
+  customer_name: string;
+  product: string;
+  district: string;
+  customer_work_area: string;
+  order_quantity: Number;
+}
+
+export const barChartData = () => {
+  const date = new Date();
+  const currentMonth = date.getMonth() + 1;
+
+  const allSales = apidata; //store.getState().allSales;
+  let dayWiseSales: any = [];
+  let allDistrict = new Set();
+
+  //expect: [ 2020-05-30: [MotoTracker: 1, EagleCam: 2], ... ]
+  allSales.map((sale: any) => {
+    const date = sale.date;
+    const district = sale.district;
+    const qty = 1;
+
+    //exclude all data other than current month
+    const saleMonth = parseInt(date.split("-")[1]);
+    if (saleMonth !== currentMonth) return null;
+
+    //insert district in a set
+    allDistrict.add(district);
+
+    let temp = [];
+    temp[district] = 1;
+
+    //Sum district wise sale on day basis
+    if (dayWiseSales[date]) {
+      if (dayWiseSales[date][district]) {
+        dayWiseSales[date][district] += qty;
+      } else {
+        dayWiseSales[date][district] = qty;
+      }
+    } else {
+      dayWiseSales[date] = temp;
+    }
+
+    return 1;
+  });
+
+  let prodWiseSale: any = [];
+  const days = Object.keys(dayWiseSales).sort();
+
+  //                    [day1 sale, day2 sale,...]
+  //expect: [AssetLock: [0,   21,  19, 13, 25, 6, 16, 0], ...]
+  days.map((day: any) =>
+    allDistrict.forEach((prod: any) => {
+      let saleCount = dayWiseSales[day][prod] ? dayWiseSales[day][prod] : 0;
+      if (prodWiseSale[prod]) {
+        prodWiseSale[prod].push(saleCount);
+      } else {
+        prodWiseSale[prod] = [saleCount];
+      }
+    })
+  );
+
+  //dataset prop
+  const datasets: any = [];
+  allDistrict.forEach((prod: any) => {
+    datasets.push({
+      label: prod,
+      backgroundColor: "#" + Math.random().toString(16).substr(-6),
+      data: prodWiseSale[prod],
+    });
+  });
+
+  //return structured barchart data
+  return {
+    labels: days,
+    datasets,
+  };
+};
---
+++ src/pages/Sales/utils/barchartData.ts
@@ -0,0 +1,84 @@
+import { apidata } from "../../../components/Charts/data";
+interface SaleProps {
+  date: string;
+  customer_name: string;
+  product: string;
+  district: string;
+  customer_work_area: string;
+  order_quantity: Number;
+}
+
+export const barChartData = () => {
+  const date = new Date();
+  const currentMonth = date.getMonth() + 1;
+
+  const allSales = apidata; //store.getState().allSales;
+  let dayWiseSales: any = [];
+  let allProduct = new Set();
+
+  //expect: [ 2020-05-30: [MotoTracker: 1, EagleCam: 2], ... ]
+  allSales.map((sale: any) => {
+    const date = sale.date;
+    const product = sale.product;
+    const qty = sale.order_quantity;
+
+    //exclude all data other than current month
+    const saleMonth = parseInt(date.split("-")[1]);
+    if (saleMonth !== currentMonth) return null;
+
+    //insert product in a set
+    allProduct.add(product);
+
+    let temp = [];
+    temp[product] = qty;
+
+    //Sum product sale on day basis
+    if (dayWiseSales[date]) {
+      if (dayWiseSales[date][product]) {
+        dayWiseSales[date][product] += qty;
+      } else {
+        dayWiseSales[date][product] = qty;
+      }
+    } else {
+      dayWiseSales[date] = temp;
+    }
+
+    return 1;
+  });
+
+  let prodWiseSale: any = [];
+  const days = Object.keys(dayWiseSales).sort();
+
+  //                    [day1 sale, day2 sale,...]
+  //expect: [AssetLock: [0,   21,  19, 13, 25, 6, 16, 0], ...]
+  days.map((day: any) =>
+    allProduct.forEach((prod: any) => {
+      let saleCount = dayWiseSales[day][prod] ? dayWiseSales[day][prod] : 0;
+      if (prodWiseSale[prod]) {
+        prodWiseSale[prod].push(saleCount);
+      } else {
+        prodWiseSale[prod] = [saleCount];
+      }
+    })
+  );
+
+  //console.log("object", prodWiseSale);
+  //console.log("lab", labels);
+  //console.log("all", dayWiseSales);
+
+  //dataset prop
+  const datasets: any = [];
+  allProduct.forEach((prod: any) => {
+    datasets.push({
+      label: prod,
+      backgroundColor: "#" + Math.random().toString(16).substr(-6),
+      data: prodWiseSale[prod],
+    });
+  });
+
+  //return structured barchart data
+  return {
+    labels: days,
+    datasets,
+  };
+};
