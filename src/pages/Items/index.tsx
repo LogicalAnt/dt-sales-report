e5c98be7c9917cb7342beeb8a822269e0a37b130
Shakil Ahmed <shakil147258@gmail.com>
2020-06-21 15:43:13 +0600

    feat: add items table

--- src/pages/Items/index.tsx
+++ src/pages/Items/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Table } from "react-bootstrap";
+import { apidata } from "../../components/Charts/data";
 import { NavigationBar } from "../../components/NavigationBar";
 import { Sidebar } from "../../components/Sidebar";
-
 export const Items = () => {
   return (
     <div className="wrapper">
@@ -11,10 +11,31 @@ export const Items = () => {
         <NavigationBar />
         <Container>
           <Row>
-            <Col md={12}>
-              <main id="content">
-                <p>Items</p>
-              </main>
+            <Col md={12} className="mt-4">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Customer Name</th>
+                    <th>Product</th>
+                    <th>District</th>
+                    <th>Customer Work area</th>
+                    <th>Order Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {apidata.map((data) => (
+                    <tr>
+                      <td>{data.date}</td>
+                      <td>{data.customer_name}</td>
+                      <td>{data.product}</td>
+                      <td>{data.district}</td>
+                      <td>{data.customer_work_area}</td>
+                      <td>{data.order_quantity}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
             </Col>
           </Row>
         </Container>
