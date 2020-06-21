10f4641b81227008aeb63d501bdbc471fae0e410
Shakil Ahmed <shakil147258@gmail.com>
2020-06-21 15:43:52 +0600

    feat: store items on fetch sales data

--- src/store/actions/sales.ts
+++ src/store/actions/sales.ts
@@ -5,6 +5,7 @@ export const fetchSales = () => (dispatch: any) => {
     .get("http://frontend.interview.dingi.work/user/data/")
     .then((response) => {
       dispatch({ type: GET_SALES, payload: response.data });
+      localStorage.setItem("salesdata", response.data);
     })
     .catch((e) => {
       dispatch({ type: GET_SALES, payload: [] });
