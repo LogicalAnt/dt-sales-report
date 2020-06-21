1c8b4a7ee6aa4fdde95d6a44fd94727eaaf407d4
Shakil Ahmed <shakil147258@gmail.com>
2020-06-21 15:42:23 +0600

    feat: add a table component

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+export const TableComponent = () => {
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
+};
